c6515e04d0208984cd57068c210d5840d0387c65
lilgoast <105431041+lilgoast@users.noreply.github.com>
2022-12-24 17:47:51 +0200

    loop hit particles
    added particles emition when player or panel behind hit loop

--- Assets/Scripts/Loops/LoopTriger.cs
+++ Assets/Scripts/Loops/LoopTriger.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 
 public class LoopTriger : MonoBehaviour
 {
+    bool loopHitted;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !loopHitted)
         {
-            Destroy(gameObject);
-            Destroy(gameObject.transform.parent.gameObject);
+            PlayParticle(0);
+            loopHitted = true;
         }
-        if(other.CompareTag("DestroyingPlane"))
+        else if(other.CompareTag("DestroyingPlane") && !loopHitted)
         {
-            Destroy(gameObject);
-            Destroy(gameObject.transform.parent.gameObject);
+            PlayParticle(1);
+            loopHitted = true;
         }
+        DestroyLoop();
     }
+
+    private void DestroyLoop()
+    {
+        Destroy(gameObject);
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+
+    private void PlayParticle(int particleIndex)  // 0 - hit, 1 - miss
+    {
+        gameObject.transform.GetChild(particleIndex).GetComponent<ParticleSystem>().Play();
+        gameObject.transform.DetachChildren();
+    }
+
+
 }
